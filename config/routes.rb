32e538df50afd04eec26b24367b33ebae02b2458
Renan Machado <renanmacs@gmail.com>
2018-01-30 18:42:42 -0200

    Creating the categories_controller methods and the new and edit views

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
+  
+  devise_for :admins
+  devise_for :members
+  root 'site/home#index'
+
   namespace :backoffice do
-    get 'categories', to: 'categories#index'
+    resources :categories, except: [:show, :destroy]
+    get 'dashboard', to: 'dashboard#index'
   end
 
   namespace :site do
     get 'home', to: 'home#index'
   end
-
-  namespace :backoffice do
-    get 'dashboard', to: 'dashboard#index'
-  end
-
-  devise_for :admins
-  devise_for :members
-  root 'site/home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

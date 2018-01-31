1be6d811602c0c937c91e68a3d14e4ed77d8141f
Renan Machado <renanmacs@gmail.com>
2018-01-31 18:20:01 -0200

    New Category View

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -4,15 +4,30 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def new
+    @category = Category.new
   end
+   
+  def create
+    @category = Category.new(params_category)
 
-   def create
+    if @category.save
+      redirect_to backoffice_categories_path, notice: "A categoria #{@category.description} foi cadastrada com sucesso!"
+    else
+      render 'new'
+    end
+
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def params_category
+    params.require(:category).permit(:description)
   end
 
 end
